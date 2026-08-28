6458fbf8271319acf5274b07774170bef5cdd1da
ataymia <ataymia.murray@allstarservicesnow.com>
2026-08-28 12:09:14 -0700

    Provide one shared HCM source of truth

--- components/momentum-app.tsx
+++ components/momentum-app.tsx
@@ -1,18 +1,15 @@
 "use client";
 
+import { HcmProvider } from "../lib/hcm-context";
 import { WorkspaceProvider, useWorkspace } from "../lib/workspace-context";
 import { AppShell } from "./app-shell";
 import { LoginScreen } from "./login-screen";
 
-function MomentumExperience() {
-  const { currentUser } = useWorkspace();
-  return currentUser ? <AppShell /> : <LoginScreen />;
+function MomentumExperience(){
+  const {currentUser}=useWorkspace();
+  return currentUser?<AppShell/>:<LoginScreen/>;
 }
 
-export function MomentumApp() {
-  return (
-    <WorkspaceProvider>
-      <MomentumExperience />
-    </WorkspaceProvider>
-  );
+export function MomentumApp(){
+  return <WorkspaceProvider><HcmProvider><MomentumExperience/></HcmProvider></WorkspaceProvider>;
 }
