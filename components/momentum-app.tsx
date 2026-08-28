64d7144f95db42785abe33621cdaf85fe8ee7760
ataymia <ataymia.murray@allstarservicesnow.com>
2026-08-28 12:59:01 -0700

    Connect marketing operations provider

--- components/momentum-app.tsx
+++ components/momentum-app.tsx
@@ -5,6 +5,7 @@ import { CommerceProvider } from "../lib/commerce-context";
 import { CrmProvider } from "../lib/crm-context";
 import { HcmProvider } from "../lib/hcm-context";
 import { InventoryLedgerProvider } from "../lib/inventory-ledger-context";
+import { MarketingProvider } from "../lib/marketing-context";
 import { PerformanceProvider } from "../lib/performance-context";
 import { WorkspaceProvider, useWorkspace } from "../lib/workspace-context";
 import { AppShell } from "./app-shell";
@@ -16,5 +17,5 @@ function MomentumExperience(){
 }
 
 export function MomentumApp(){
-  return <WorkspaceProvider><CrmProvider><HcmProvider><PerformanceProvider><CommerceProvider><InventoryLedgerProvider><AccountingProvider><MomentumExperience/></AccountingProvider></InventoryLedgerProvider></CommerceProvider></PerformanceProvider></HcmProvider></CrmProvider></WorkspaceProvider>;
+  return <WorkspaceProvider><CrmProvider><HcmProvider><PerformanceProvider><CommerceProvider><InventoryLedgerProvider><AccountingProvider><MarketingProvider><MomentumExperience/></MarketingProvider></AccountingProvider></InventoryLedgerProvider></CommerceProvider></PerformanceProvider></HcmProvider></CrmProvider></WorkspaceProvider>;
 }
