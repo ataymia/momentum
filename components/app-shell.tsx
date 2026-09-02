9aeb4b1df8791c483a4761842cb05b562b31436d
ataymia <ataymia.murray@allstarservicesnow.com>
2026-09-02 07:33:30 -0700

    Attach account profile enhancement layer

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -1,1 +1,8 @@
-export { AppShell } from "./app-shell-v3";
+"use client";
+
+import { AppShell as BaseAppShell } from "./app-shell-v3";
+import { EnhancementLayer } from "./enhancement-layer";
+
+export function AppShell(){
+  return <><BaseAppShell/><EnhancementLayer/></>;
+}
