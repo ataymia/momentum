31bb5ab9068493ec2e8446d8b30f6c9eeb286b51
ataymia <ataymia.murray@allstarservicesnow.com>
2026-09-02 07:34:56 -0700

    Expose warehouse worker movement and count workspace

--- components/inventory/inventory-ledger-panel.tsx
+++ components/inventory/inventory-ledger-panel.tsx
@@ -2,5 +2,6 @@
 
 import { InventoryLedgerPanel as InventoryLedgerPanelV2 } from "./inventory-ledger-panel-v2";
 import { WarehouseControlPanel } from "./warehouse-control-panel";
+import { WarehouseOperationsPanel } from "./warehouse-operations-panel";
 
-export function InventoryLedgerPanel(){return <><WarehouseControlPanel/><InventoryLedgerPanelV2/></>;}
+export function InventoryLedgerPanel(){return <><WarehouseControlPanel/><WarehouseOperationsPanel/><InventoryLedgerPanelV2/></>;}
