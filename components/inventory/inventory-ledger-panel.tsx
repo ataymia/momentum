c59d03728b3fd4dff9babeed5c4a4e5e7c332c83
ataymia <ataymia.murray@allstarservicesnow.com>
2026-09-02 07:34:08 -0700

    Add warehouse stock controls above custody ledger

--- components/inventory/inventory-ledger-panel.tsx
+++ components/inventory/inventory-ledger-panel.tsx
@@ -1,1 +1,6 @@
-export { InventoryLedgerPanel } from "./inventory-ledger-panel-v2";
+"use client";
+
+import { InventoryLedgerPanel as InventoryLedgerPanelV2 } from "./inventory-ledger-panel-v2";
+import { WarehouseControlPanel } from "./warehouse-control-panel";
+
+export function InventoryLedgerPanel(){return <><WarehouseControlPanel/><InventoryLedgerPanelV2/></>;}
